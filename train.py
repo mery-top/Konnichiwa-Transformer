c004f27565d86a6b3e8d04681f3d6a6df4ec17d4
Meerthika <meerthikasrrajendrans@gmail.com>
2026-08-16 19:13:45 +0530

    Implement the train function

---
+++ train.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from tokenizer import SimpleTokenizer
+from dataset import PAIRS, TranslationDataset
+from model import Transformer
+
+
+def train_transformer():
+
+    device = torch.device(
+        "mps" if torch.backends.mps.is_available()
+        else "cuda" if torch.cuda.is_available()
+        else "cpu"
+    )
+
+
+    MAX_SEQ_LEN = 20
+    BATCH_SIZE = 16
+    D_MODEL = 512
+    NUM_HEADS = 8      # h = 8 heads
+    NUM_LAYERS = 6     # N = 6 layers (original paper uses 6, we use 3 for faster CPU training)
+    D_FF = 2048         # Feed-forward hidden dimension
+    DROPOUT = 0.1
+    LEARNING_RATE = 5e-4
+    EPOCHS = 60
+
+
+    src_tokenizer = SimpleTokenizer()
+    tgt_tokenizer = SimpleTokenizer()
+
+    english_sentences = []
+    japanese_sentences = []
+
+    for english, japanese in PAIRS:
+        english_sentences.append(english)
+       japanese_sentences.append(japanese)
+
+    src_tokenizer.build_vocab(english_sentences)
+    tgt_tokenizer.build_vocab(japanese_sentences)
+
+
+    dataset = TranslationDataset(
+    PAIRS,
+    src_tokenizer,
+    tgt_tokenizer,
+    max_seq_len=MAX_SEQ_LEN,
+    )
+
+    loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True) #he model shouldn't always see the training examples in exactly the same order.
+
+
+    model = Transformer(
+        src_vocab_size=len(src_tokenizer.vocab),
+        tgt_vocab_size=len(tgt_tokenizer.vocab),
+        d_model=D_MODEL,
+        num_heads=NUM_HEADS,
+        d_ff=D_FF,
+        num_layers=NUM_LAYERS,
+    ).to(device)
+
+
+    pad_id = tgt_tokenizer.word2idx[tgt_tokenizer.pad_token]
+
+
+    """
+
+                MODEL
+                  ↓
+             prediction
+                  ↓
+             loss function
+                  ↓
+             calculate loss
+                  ↓
+          calculate gradients
+                  ↓
+              OPTIMIZER
+                  ↓
+        change model parameters (based on the lr)
+                  ↓
+             better model
+    """
+
+    loss_function = nn.CrossEntropyLoss(ignore_index=pad_id) #dont want the model to learn paddings
+    optimizer = torch.optim.Adam(model.parameter(), lr=LEARNING_RATE)
+
+    """
+                     MODEL
+                   ↓
+              logits
+                   ↓
+              [2, 4, 10]
+                   ↓
+             reshape(-1, 10)
+                   ↓
+              [8, 10]
+                   ↓
+              ┌─────────┐
+              │  LOSS   │
+              └─────────┘
+                   ↑
+              [8] target             Prediction 1 → 10 possible tokens
+                                    Prediction 2 → 10 possible tokens
+                                    Prediction 3 → 10 possible tokens
+                                    ...
+                                    Prediction 8 → 10 possible tokens
+                   ↑
+             tgt_label
+               [2, 4]
+                   ↓
+              reshape(-1)
+                   ↓
+                 [8]
+    """
+
+    model.train()
+
+for epoch in range(EPOCHS):
+    total_loss = 0.0
+
+    for batch in loader:
+        src = batch["src"].to(device)
+        tgt_input = batch["tgt_input"].to(device)
+        tgt_label = batch["tgt_label"].to(device)
+        src_mask = batch["src_mask"].to(device)
+        tgt_mask = batch["tgt_mask"].to(device)
+
+        logits = model(src, tgt_input, src_mask, tgt_mask)
+
+        loss = loss_function(
+            logits.reshape(-1, logits.size(-1)),
+            tgt_label.reshape(-1),
+        )
+
+        optimizer.zero_grad() #clear old gradienst
+        loss.backward() #calculate new loss gradients
+        optimizer.step() #redo the values
+
+        total_loss += loss.item()
+
+    if (epoch + 1) % 20 == 0:
+        average_loss = total_loss / len(loader)
+        print(f"Epoch {epoch + 1}: loss = {average_loss:.4f}")
+
+
+
+
